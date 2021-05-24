dc2c27e741c97f80c84a6c23d43d354da9177011
PerplexityCoding <ymenard.dev@gmail.com>
2021-05-24 15:22:28 +0200

    test

---
+++ from_json.js
@@ -0,0 +1,51 @@
+const bent = require('bent');
+const centerIds = [ 4422387, 4869765, 4790726, 4789625, 4792051 ];
+const getJSON = bent('json');
+const open = require('open');
+
+const doctoUrl = (id) => `https://www.doctolib.fr/search_results/${id}.json?ref_visit_motive_ids%5B%5D=6970&ref_visit_motive_ids%5B%5D=7005&speciality_id=5494&search_result_format=json&force_max_limit=2`;
+
+run();
+
+async function run(cpt = 1) {
+    console.log(`checking ${cpt} ...`);
+    try {
+        const list = await getDoses();
+        if (list.length > 0) {
+            console.log('\x07', list.map(item => `${item.name}\n${item.url}`).join('\n'));
+            list.forEach((item) => {
+                open(`https://www.doctolib.fr/${item.url}`);
+            });
+        }
+
+        await waitFor(1000);
+    } catch (e) {
+        console.error(e);
+    } finally {
+        run(cpt + 1);
+    }
+}
+
+function waitFor(to) {
+    return new Promise((success) => {
+        setTimeout(() => {
+            success();
+        }, to);
+    });
+}
+
+async function getDoses() {
+    const doses = [];
+    for (const centerId of centerIds) {
+        const url = doctoUrl(centerId);
+        const data = await getJSON(url);
+
+        if (data && data.reason != 'no_availabilities') {
+            console.log(data);
+            doses.push(data.search_result);
+        }
+
+        await waitFor(1000);
+    }
+    return doses;
+}
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const puppeteer = require('puppeteer');
+const open = require('open');
 
-const URL = process.argv[2] || 'https://www.doctolib.fr/vaccination-covid-19/nantes?ref_visit_motive_ids[]=6970&ref_visit_motive_ids[]=7005&force_max_limit=2';
+const URL = process.argv[2] || 'https://www.doctolib.fr/vaccination-covid-19/vanves?ref_visit_motive_ids[]=6970&ref_visit_motive_ids[]=7005&force_max_limit=2';
+
+// const URL = process.argv[2] || 'https://www.doctolib.fr/vaccination-covid-19/vanves?ref_visit_motive_ids[]=6970&ref_visit_motive_ids[]=7005&force_max_limit=2';
 
 console.log(`URL: ${URL}`);
 
@@ -12,6 +15,9 @@ async function run(cpt = 1) {
     const list = await getDoses();
     if (list.length > 0) {
       console.log('\x07', list.map(item => `${item.name}\n${item.url}`).join('\n'));
+      list.forEach((item) => {
+        open(item.url);
+      });
     }
   } catch (e) {
     console.error(e);
@@ -20,18 +26,51 @@ async function run(cpt = 1) {
   }
 }
 
+function waitFor(to) {
+  return new Promise((sucess) => {
+    setTimeout(() => {
+      sucess();
+    }, to);
+  });
+}
+
 async function getDoses() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setCacheEnabled(false);
   await page.setViewport({ width: 1600, height: 5000 });
   await page.goto(URL, { waitUntil: 'networkidle0' });
-  await page.screenshot({ path: 'screen.png' });
-  const list = await page.evaluate(() =>
-    Array.from(document.querySelectorAll('.dl-search-result'))
+
+  await page.evaluate(() => new Promise((resolve) => {
+    let scrollTop = -1;
+    const interval = setInterval(() => {
+      window.scrollBy(0, 100);
+      if(document.documentElement.scrollTop !== scrollTop) {
+        scrollTop = document.documentElement.scrollTop;
+        return;
+      }
+      window.scrollTo(0, 0);
+      clearInterval(interval);
+      resolve();
+    }, 50);
+  }));
+  await waitFor(4000);
+
+  const list = await page.evaluate(() => {
+    const blacklist = ['Saint-Mandé'];
+    const whitelist = ['Porte de Versailles', 'Paris 14', 'Paris 15', 'Malakoff', 'Issy', 'Pasteur', 'Montrouge', 'Dunant'];
+    return Array.from(document.querySelectorAll('.dl-search-result'))
       .filter(element => !element.innerHTML.includes('Aucun rendez-vous'))
+      .filter(element => !blacklist.some((e) => element.innerHTML.includes(e)))
+      .filter(element => whitelist.some((e) => element.innerHTML.includes(e)))
       .map(element => element.querySelector('.dl-search-result-name'))
-      .map(element => ({ name: element.innerText.trim(), url: element.href })));
+      .map(element => ({ name: element.innerText.trim(), url: element.href }))
+  });
+
+  if (list.length > 0) {
+    await page.screenshot({ path: 'screen_found.png' });
+  }
+
   await browser.close();
   return list;
 }
